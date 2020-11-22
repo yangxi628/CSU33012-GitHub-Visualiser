c902c88e848fb84ddcf0b126872a2a572eff8c6b
Ahmed <ahmedhamedaly19@gmail.com>
2020-11-22 20:06:36 +0000

    changed colors again

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-// handleInput() -> main(user:string)
+// handleInput() -> main(user:string, token:string)
 function handleInput() {
     var user = document.getElementById("user").value;
-    main(user);
+    var token = document.getElementById("token").value !== "" ? document.getElementById("token").value : undefined;
+    console.log(token);
+    console.log(user)
+    main(user, token);
 }
 
 // getRequest(url:string) -> data:json
@@ -11,8 +14,8 @@ async function getRequest(url) {
     return data;
 }
 
-// main(user:string)
-async function main(user) {
+// main(user:string, token:string)
+async function main(user, token) {
     let url = `https://api.github.com/users/${user}/repos`;
     let repo = await getRequest(url).catch(error => console.error(error));
 
