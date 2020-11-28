0dcc5f728a0615e0062e90a4d7df33dd0ca6bbb9
Ahmed <ahmedhamedaly19@gmail.com>
2020-11-28 14:16:15 +0000

    graph for languages

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 function handleInput() {
     var user = document.getElementById("user").value;
     var token = document.getElementById("token").value !== "" ? document.getElementById("token").value : undefined;
-    
+
     main(user, token);
 }
 
@@ -10,14 +10,14 @@ function handleInput() {
 async function getRequest(url, token) {
 
     const headers = {
-        'Authorization':`Token ${token}`
+        'Authorization': `Token ${token}`
     }
 
     const response = (token == undefined) ? await fetch(url) : await fetch(url, {
         "method": "GET",
         "headers": headers
     });
-    
+
     let data = await response.json();
     return data;
 }
@@ -28,17 +28,80 @@ async function main(user, token) {
 
     let repo = await getRequest(url, token).catch(error => console.error(error));
 
-    commitsOverTime(repo, user, token);
-    console.log(repo);    
+    language(repo, user, token);
 }
 
-async function commitsOverTime(repo, user, token) {
-    
+async function language(repo, user, token) {
+    let label = [];
+    let data = [];
+    let backgroundColor = [];
+
     for (i in repo) {
-        let url = `https://api.github.com/repos/${user}/${repo[i].name}/commits`;
-        let commits = await getRequest(url, token).catch(error => console.error(error));
-        console.log(repo[i].name + ': ' + commits.length);
+        let url = `https://api.github.com/repos/${user}/${repo[i].name}/languages`;
+        let languages = await getRequest(url, token).catch(error => console.error(error));
+
+        for (language in languages) {
+
+            if (label.includes(language)) {
+                for (i = 0; i < label.length; i++)
+                    if (language == label[i])
+                        data[i] = data[i] + languages[language];
+                    
+            } else {
+                label.push(language);
+                data.push(languages[language]);
+                backgroundColor.push(`rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 0.2)`);
+            }
+        }
+
     }
     
-    
+    draw('myChart', 'pie', 'languages', 'User Languages (in bytes)', label, data, backgroundColor);
+
+}
+
+function draw(ctx, type, datasetLabel, titleText, label, data, backgroundColor) {
+    let myChart = document.getElementById(ctx).getContext('2d');
+
+    new Chart(myChart, {
+        type: type,
+        data: {
+            labels: label,
+            datasets: [{
+                label: datasetLabel,
+                data: data,
+                backgroundColor: backgroundColor,
+                borderWidth: 1,
+                borderColor: '#777',
+                hoverBorderWidth: 2,
+                hoverBorderColor: '#000'
+            }],
+
+        },
+        options: {
+            title: {
+                display: true,
+                text: titleText,
+                fontSize: 20
+            },
+            legend: {
+                display: true,
+                position: 'bottom',
+                labels: {
+                    fontColor: '#000'
+                }
+            },
+            layout: {
+                padding: {
+                    left: 50,
+                    right: 0,
+                    bottom: 0,
+                    top: 0
+                }
+            },
+            tooltips: {
+                enabled: true
+            }
+        }
+    });
 }
